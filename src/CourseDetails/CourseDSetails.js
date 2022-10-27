08d3f71ed1c64853082e6e69322c128afb7df23a
Ismailjoney <mohammadismailjoney@gmail.com>
2022-10-27 17:23:51 +0600

    courseDetails change

--- src/CourseDetails/CourseDSetails.js
+++ src/CourseDetails/CourseDSetails.js
@@ -9,26 +9,7 @@ const CourseDSetails = () => {
  
      
     return (
-        <div  style={{ width: '80%',margin: '0 auto' }}>
-            <h2 className='mt-4 mb-5'>{header}</h2>
-            <Card style={{ width: '100%' }}>
-                <Card.Img style={{ width: '100%',height: '20rem' }} variant="top" src={ img} />
-                <Card.Body>
-                    <Card.Title>{ name}</Card.Title>
-                    <Card.Text>
-                        { details}
-                        <h6 className='mt-4'>Addmision Last Date {admisionlastdate}</h6>
-                        <h6>Course Fee : { coursefee}</h6>
-                        <h6>Course start :{ coursestart}</h6>
-                        <h6>Place :{ Place}</h6>
-                    </Card.Text>
-                    <Button className='bg-dark'variant="primary">
-                        <Link to={`/checkout/${ id}`}>Premium</Link> 
-                    </Button>
-                </Card.Body>
-            </Card>
-
-        </div>
+        
     );
 };
 

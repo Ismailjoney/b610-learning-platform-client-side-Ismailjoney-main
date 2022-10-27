0760bea95e9fab06be4b499837d0d27fd208e25d
Ismailjoney <mohammadismailjoney@gmail.com>
2022-10-27 17:09:40 +0600

    aded change

--- src/PrivetRoute/CheckOut.js
+++ src/PrivetRoute/CheckOut.js
@@ -4,11 +4,7 @@ import { useLoaderData } from 'react-router-dom';
  const CheckOut = () => {
     const data = useLoaderData();
     console.log(data)
-    return (
-        <div>
-            <h2> Checkout Your Premium course</h2>
-        </div>
-    );
+    
  };
  
  export default CheckOut;
